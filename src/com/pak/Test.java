709565af8b03ba83c91f4d26721927b910dfbece
ujjawalgit <57063174+ujjawalgit@users.noreply.github.com>
2021-07-07 21:39:12 +0530

    2ndb

--- src/com/pak/Test.java
+++ src/com/pak/Test.java
@@ -6,5 +6,8 @@ public class Test {
         Test1 test1 = new Test1();
         int sum = test1.getSum(11,11);
         System.out.println("sum:"+sum);
+        Test2 test2 = new Test2();
+        int sub = test2.getSubstraction(11,10);
+        System.out.println("substraction="+sub);
     }
 }
---
+++ src/com/pak/Test2.java
@@ -0,0 +1,7 @@
+package com.pak;
+
+public class Test2 {
+    public int getSubstraction(int i, int i1) {
+        return i-i1;
+    }
+}
