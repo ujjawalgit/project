8d08033b0b316dc5b5f89935be5f2e3da47de6d1
ujjawalgit <57063174+ujjawalgit@users.noreply.github.com>
2021-07-07 21:28:51 +0530

    1stb

---
+++ src/com/pak/Test.java
@@ -0,0 +1,10 @@
+package com.pak;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("welcome");
+        Test1 test1 = new Test1();
+        int sum = test1.getSum(11,11);
+        System.out.println("sum:"+sum);
+    }
+}
---
+++ src/com/pak/Test1.java
@@ -0,0 +1,7 @@
+package com.pak;
+
+public class Test1 {
+    public int getSum(int i, int i1) {
+        return i + i1;
+    }
+}
